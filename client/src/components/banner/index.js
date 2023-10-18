a935a037fe280d25ad3d8a8aa64f75dbab18bc13
Site Builders <sitebldrs@gmail.com>
2023-10-18 13:49:25 +0100

    updated

--- client/src/components/banner/index.js
+++ client/src/components/banner/index.js
@@ -3,7 +3,13 @@ import React from "react";
 export const BannerComponent = ({ title, subtitle }) => {
   return (
     <React.Fragment>
-      <h1 className="title" dangerouslySetInnerHTML={{ __html: title }} />
+      <h1
+        className="title"
+        style={{
+          fontWeight: "900",
+        }}
+        dangerouslySetInnerHTML={{ __html: title }}
+      />
       <p className="subtitle">{subtitle}</p>
     </React.Fragment>
   );
